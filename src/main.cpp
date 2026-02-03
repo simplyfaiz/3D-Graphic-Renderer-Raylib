6135d9a2d192a032979f6adf440aa7fa2fdd1c2e
builtbyfaiz <dev.faiz@hotmail.com>
2026-02-03 13:21:10 +0500

    push cube backwards out of {0,0,0] to see it

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,19 @@ int main()
     raylib::Window graphicWindow(600, 600, "Graphic Render", FLAG_VSYNC_HINT);
     SetTargetFPS(75);
 
-    float h = 100.f; // Distance from Center {0,0,0} of the cube's faces, Used to create symmetric cube
+    float h =
+        100.f; // Distance from Center {0,0,0} of the cube's faces, Used to create symmetric cube
+
     vector<Point3D> cube = {{-h, -h, -h}, {h, -h, -h}, {h, h, -h}, {-h, h, -h},
                             {-h, -h, h},  {h, -h, h},  {h, h, h},  {-h, h, h}};
 
     vector<Point2D> points2D;
+
+    // Cube is at {0,0,0}, Camera is also at [0,0,0] so, Push cube forward to see it better
+    for (auto &p : cube)
+    {
+        p.z += 300;
+    }
 
     while (!WindowShouldClose())
     {
@@ -24,7 +32,7 @@ int main()
 
         for (auto &p : cube)
         {
-            p.x += 0.01;
+            p.x += 0.05;
             Renderer::render(p);
         }
 
