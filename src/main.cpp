6f3ba80428d57329dbbbafd29161464e89939502
Faiz <faizr9315@gmail.com>
2026-03-20 07:15:46 +0500

    bugfix, fix world floating away

--- src/main.cpp
+++ src/main.cpp
@@ -11,49 +11,23 @@ int main()
 {
     raylib::Window graphicWindow(600, 600, "Graphic Render", FLAG_VSYNC_HINT);
     SetTargetFPS(75);
-
-    float h = 100.f; // Cube face distance from origin, Used to create symmetric cube
-    Shape cube, cube2;
-
-    cube.vertices = {
-        
-        {-h, -h, -h}, {h, -h, -h}, {h, h, -h}, {-h, h, -h},
-        {-h, -h,  h}, {h, -h,  h}, {h, h,  h}, {-h, h,  h}
-    };
-
-    cube2.vertices = cube.vertices;
-
-    cube.edges = {
-        {0, 1}, {1, 2}, {2, 3}, {3, 0}, // back face
-        {4, 5}, {5, 6}, {6, 7}, {7, 4}, // front face
-        {0, 4}, {1, 5}, {2, 6}, {3, 7}  // connecting edges
-    };
-
-    cube2.edges = cube.edges;
-    cube2.setColor(RED);
-
-    cube.saveShape("resources/cube.txt");
-    cube2.saveShape("resources/cube2.txt");
-
-    Shape mew, mew2;
-    mew.loadShape("resources/cube.txt");
-    mew2.loadShape("resources/cube2.txt");
     
-    // create a copy of original world to turn into camera view #TODO make this automatic in render pipeline
+    Shape cube1, cube2;
+    
+    
+    cube1.loadShape("resources/cube.txt");
+    cube2.loadShape("resources/cube2.txt");
+   
     World world;
-    world.shapes.push_back(mew);
-    world.shapes.push_back(mew2);
+    world.shapes.push_back(cube1);
+    world.shapes.push_back(cube2);
 
-
-    World worldToRender = world; 
-    
+    Renderer::pushWorldIntoView(world, 600);// Temp function
     while (!WindowShouldClose())
     {
         Controller::handleInput();
         Controller::update(world);
-        worldToRender = world;
-        Renderer::pushWorldIntoView(worldToRender, 600);// Temp function
-        Renderer::render(worldToRender);
+        Renderer::render(world);
     }
     return 0;
 }
--- src/renderer.h
+++ src/renderer.h
@@ -1,33 +1,31 @@
 #pragma once
-#include "vector3D.h"
 #include "globals.h"
 #include "raylib-cpp.hpp"
+#include "vector3D.h"
 #include "world.h"
 
-// clang-format off
-class Renderer
+  // clang-format off
+  // clang-format on
+namespace Renderer
 {
-  public:
-    static float focal;
+extern float focal;
 
-    static void transformPoint(Vec3 &p);          // Convert point to cartesian plane, so projection is accurate
-    static void projectPoint(Vec3 &p);            // Project 3D Point on 2d screen
-    static void preparePoint(Vec3 &p);            // Transforms and projects a 3D point
-    static void drawPoint(Vec3 &p, Color color);  // Renders point on 2d screen's cartesian plane
-    
-    static void transformShape(Shape &shape);  // Rotate, Scale, Translate shape in 3D space
-    static void projectShape(Shape &shape);    // Project 3D shape on 2D screen's cartesian plane
-    static void drawShape(Shape &shape);       // Renders 3D shape on 2D screen's cartesian plane
-    
-    static void transformWorld(World &world);  // Rotate, Scale, Translate shapes of world in 3D space
-    static void projectWorld(World &world);    // Project 3D world to 2D screen's cartesian plane
-    static void prepareWorld(World &world);    // Transforms and projects all shapes inside a world
-    static void drawWorld(World &world);       // Renders 3D world on 2D screen's cartesian plane
+void transformPoint(Vec3 &p);          // Convert point to cartesian plane, so projection is accurate
+void projectPoint(Vec3 &p);            // Project 3D Point on 2d screen
+void preparePoint(Vec3 &p);            // Transforms and projects a 3D point
+void drawPoint(Vec3 &p, Color color);  // Renders point on 2d screen's cartesian plane
 
-    static void render(World &world);  // Render world via render pipeline, Transform->Project->Draw
-    
-    // TEMP DEBUG FUCTION
-    static void pushWorldIntoView(World &world, int amount);
+void transformShape(Shape &shape);  // Rotate, Scale, Translate shape in 3D space
+void projectShape(Shape &shape);    // Project 3D shape on 2D screen's cartesian plane
+void drawShape(Shape &shape);       // Renders 3D shape on 2D screen's cartesian plane
 
-    // clang-format on
-};
+void transformWorld(World &world);  // Rotate, Scale, Translate shapes of world in 3D space
+void projectWorld(World &world);    // Project 3D world to 2D screen's cartesian plane
+void prepareWorld(World &world);    // Transforms and projects all shapes inside a world
+void drawWorld(World &world);       // Renders 3D world on 2D screen's cartesian plane
+
+void render(World &world);  // Render world via render pipeline, Transform->Project->Draw
+
+  // TEMP DEBUG FUCTION
+void pushWorldIntoView(World &world, int amount);
+}
